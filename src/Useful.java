c6f08158c509344d7c7c3d4e5b4b125f1ce41dc6
Bogdan <bandrashko1@gmail.com>
2022-07-08 17:59:45 +0200

    added TextField example

--- src/Useful.java
+++ src/Useful.java
@@ -205,4 +205,43 @@ public class Useful {
 
 //         layeredPane.add(label1, Integer.valueOf(0));
 
+
+    // TEXT field
+
+    // public class MyFrame extends JFrame implements ActionListener {
+    //
+    //    JButton button;
+    //    JTextField textField;
+    //
+    //    MyFrame(){
+    //        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //        this.setLayout(new FlowLayout());
+    //
+    //        button = new JButton("Submit");
+    //        button.addActionListener(this);
+    //
+    //        textField = new JTextField();
+    //        textField.setPreferredSize(new Dimension(250, 40));
+    //        textField.setFont(new Font("Consolas", Font.BOLD, 35));
+    //        textField.setForeground(new Color(0x00ff00));
+    //        textField.setBackground(Color.red);
+    //        textField.setCaretColor(Color.orange);
+    //        textField.setToolTipText("username");
+    //
+    //        this.add(button);
+    //        this.add(textField);
+    //        this.pack();
+    //        this.setVisible(true);
+    //    }
+    //
+    //    @Override
+    //    public void actionPerformed(ActionEvent e) {
+    //        if (e.getSource()==button){
+    //            System.out.println("Welcome " + textField.getText());
+    //            textField.setEditable(false);
+    //            button.setEnabled(false);
+    //        }
+    //    }
+    //}
+
 }
