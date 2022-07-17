ed429a439235e3094e4433b0e653527e6f88513d
Bogdan <bandrashko1@gmail.com>
2022-07-17 22:14:56 +0200

    Learning about Inner Classes and Anonymous Inner Classes

---
+++ src/Greeting.java
@@ -0,0 +1,19 @@
+public class Greeting {
+
+    public void Welcome(){
+        System.out.println("Hello world!");
+    }
+
+//    Greeting greeting = new Greeting(){
+//        @Override
+//        public void Welcome(){
+//            System.out.println("Yo bro!");
+//        }
+//    };
+//
+//    Greeting greeting2 = new Greeting();
+//
+//        greeting.Welcome();
+//        greeting2.Welcome();
+
+}
